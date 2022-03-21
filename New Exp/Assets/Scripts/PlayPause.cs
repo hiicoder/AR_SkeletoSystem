67a85793d856ce41b1233553aa8ad26d127d4a33
Himanshu kumar <49545079+hiicoder@users.noreply.github.com>
2022-03-21 15:11:07 +0530

    first commit
    AR Experience Skeleton System

---
+++ New Exp/Assets/Scripts/PlayPause.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class PlayPause : MonoBehaviour
+{
+    private VideoPlayer video;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        video = GetComponent<VideoPlayer>();
+    }
+    public void PlayVideo()
+    {
+        video.Play();
+    }
+    public void PauseVideo()
+    {
+        video.Pause();
+    }
+}
